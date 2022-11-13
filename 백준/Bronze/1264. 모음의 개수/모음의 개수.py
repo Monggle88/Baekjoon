840d108783c79f83844a41a145f8e7d71ffd5012
Monggle88 <106243912+Monggle88@users.noreply.github.com>
2022-11-13 17:46:53 +0900

    Update 모음의 개수.py
    정규 표현식 사용

--- 백준/Bronze/1264. 모음의 개수/모음의 개수.py
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -1,30 +1,12 @@
 import sys
+import re
 
-inputArray = []
-sentence = ""
+pattern = re.compile('[aeiou]')
 
-# while sentence != "#":
-#     sentence = sys.stdin.readline()
-#     if sentence != "#":
-#         inputArray.append(sentence)
+while True:
+    inputStr = sys.stdin.readline().strip().lower()
+    if inputStr == "#": break
 
-while sentence != "#":
-    sentence = input().lower()
-    if sentence != "#":
-        inputArray.append(sentence)
+    result = pattern.findall(inputStr)
 
-vowel = ['a', 'e', 'i', 'o', 'u']
-
-resultArray = []
-
-for str in inputArray:
-    result = 0
-
-    for i in range(len(str)):
-        if str[i] in vowel:
-            result += 1
-
-    resultArray.append(result)
-
-for i in range(len(resultArray)):
-    print(resultArray[i])
+    print(len(result))
