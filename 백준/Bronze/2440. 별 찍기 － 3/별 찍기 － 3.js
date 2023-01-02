7c0331a0801e9578adcf32738e4ce7e3ba5c26b9
Monggle88 <106243912+Monggle88@users.noreply.github.com>
2023-01-02 18:50:10 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 120 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = Number(fs.readFileSync('/dev/stdin').toString().trim());
+
+let result = '';
+
+for (let i = input; i > 0; i--) {
+    result += '*'.repeat(i);
+    result += '\n';
+}
+
+console.log(result);
